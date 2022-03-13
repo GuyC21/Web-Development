12affb468629b3a82af25e3bec620e99118e4ce3
GuyC21 <85683040+GuyC21@users.noreply.github.com>
2022-03-13 15:24:41 +0200

    Delete CodePlayerJS.js

--- CodePlayerJS.js
+++
@@ -1,7 +0,0 @@
-//alerting if jQuery isnt working as intended
-if (typeof jQuery == "undefined") {
-    alert('jQuery not working as intended. You may face problems');
-}
-
-//setting an accordion up
-$("#accordion").accordion();
