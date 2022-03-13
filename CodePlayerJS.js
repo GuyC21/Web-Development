59a906f0369e781e6a43b6ad15bff1c9b022124e
GuyC21 <85683040+GuyC21@users.noreply.github.com>
2022-03-13 15:21:27 +0200

    Add files via upload

---
+++ CodePlayerJS.js
@@ -0,0 +1,7 @@
+//alerting if jQuery isnt working as intended
+if (typeof jQuery == "undefined") {
+    alert('jQuery not working as intended. You may face problems');
+}
+
+//setting an accordion up
+$("#accordion").accordion();
